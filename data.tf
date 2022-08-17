6f257085cf2f9602b166462f0b5957e28ca209b4
hussainsheriffC <hussain.sheriff@cloudreach.com>
2022-08-17 16:15:09 +0530

    adding ec2 files

--- data.tf
+++ data.tf
@@ -13,7 +13,35 @@ data "aws_ami" "ubuntu" {
 
 data "aws_subnet" "Elk" {
     filter {
-        name   = "tag:Name"
-        values = ["public_subnet"]
+      name   = "tag:Name"
+      values = ["public_subnet"]
   } 
+}
+
+data "aws_vpc" "elk_vpc"{
+  filter {
+    name = "tag:Name"
+    values = ["Elk-vpc"]
+  }
+}
+
+data "aws_internet_gateway" "elk_igw" {
+  filter {
+    name = "tag:Name"
+    values="Internet gateway"
+  }
+  
+}
+
+data "aws_ami" "kibana" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  #we removed few lines we don't need it (hvm)
+
+  owners = ["099720109477"] # Canonical
 }
--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "test_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.medium"
+# resource "aws_instance" "test_server" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t3.medium"
 
-  vpc_security_group_ids = [aws_security_group.test_server.id]
-  key_name = "talent-academy-lab"
-  subnet_id = data.aws_subnet.Elk.id
+#   vpc_security_group_ids = [aws_security_group.test_server.id]
+#   key_name = "talent-academy-lab"
+#   subnet_id = data.aws_subnet.Elk.id
 
 
-  tags = {
-    Name = "Test Server"
-  }
-}
+#   tags = {
+#     Name = "Test Server"
+#   }
+# }
 
 resource "aws_eip" "elk_test_ip" {
   instance = aws_instance.test_server.id
