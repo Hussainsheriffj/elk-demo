f77c4e292da536278862c5629fd97094e9a874aa
hussainsheriffC <hussain.sheriff@cloudreach.com>
2022-08-18 15:42:29 +0530

    adding ec2

--- data.tf
+++ data.tf
@@ -28,20 +28,51 @@ data "aws_vpc" "elk_vpc"{
 data "aws_internet_gateway" "elk_igw" {
   filter {
     name = "tag:Name"
-    values = "Internet gateway"
+    values =  "Internet gateway"
   }
   
 }
 
-data "aws_ami" "kibana" {
+data "aws_ami" "kibana_id" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["kibana"]
   }
 
   #we removed few lines we don't need it (hvm)
 
   owners = ["099720109477"] # Canonical
+}
+
+data "aws_ami" "logstach_id" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["logstach"]
+  }
+
+  #we removed few lines we don't need it (hvm)
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_ami" "elasticsearch" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["elasticsearch"]
+  }
+
+  #we removed few lines we don't need it (hvm)
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_key_pair" "eu_key" {
+  key_name = "talent-academy-lab"
+  include_public_key = true
 }
